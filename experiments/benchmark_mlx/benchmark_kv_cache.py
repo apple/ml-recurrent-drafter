59ec955a8b91aa12553f3ddb653c3bef4dc4357f
Yunfei Cheng <yunfeic414@gmail.com>
2024-06-05 22:11:01 -0700

    Benchmark mlx llama forward and kv cache (#54)
    * [feat] Add experiments folder. Benchmark llama and kv cache
    
    * [feat] benchmark llama
    
    * [style]
    
    * [style]
    
    * Update mlx_recurrent_drafting/experiments/benchmark_mlx/benchmark_llama.py
    
    Co-authored-by: Yi Wang <yi_wang999747@apple.com>
    
    ---------
    
    Co-authored-by: Yunfei Cheng <yunfeic@Yunfeis-MacBook-Pro.local>
    Co-authored-by: Yi Wang <yi_wang999747@apple.com>

---
+++ experiments/benchmark_mlx/benchmark_kv_cache.py
@@ -0,0 +1,77 @@
+import time
+
+import mlx.core as mx
+import mlx_lm
+
+from mlx_recurrent_drafting import kv_cache
+
+
+def benchmark_kv_cache_cat():
+    n_layer, n_kv_heads = 1, 32
+    head_dim = 4096 // n_kv_heads
+    max_len = 100
+    batch_size = 1
+    v = mx.ones([batch_size, n_kv_heads, 1, head_dim]).astype(mx.bfloat16)
+    mx.eval(v)
+    print(f"num iterations = {max_len}")
+
+    # ref kv
+    ref_kv_cache = mlx_lm.models.base.KVCache(head_dim, n_kv_heads)
+    mx.eval(ref_kv_cache.keys, ref_kv_cache.values)
+    tic = time.perf_counter()
+    for _ in range(max_len):
+        ref_kv_cache.update_and_fetch(keys=v, values=v)
+    mx.eval(ref_kv_cache.keys, ref_kv_cache.values)
+    toc = time.perf_counter()
+    tpi = 1e3 * (toc - tic) / max_len
+    print(f"ref_kv Time per iteration {tpi:.3f} (ms)")
+
+    # test kv
+    test_kv_cache = kv_cache.Cache(
+        batch_size, max_len, n_layer, n_kv_heads, head_dim, dtype=mx.bfloat16
+    )
+    mx.eval(test_kv_cache._cache)
+    tic = time.perf_counter()
+    for _ in range(max_len):
+        test_kv_cache.sliced[0][0].cat(v)  # key
+        test_kv_cache.sliced[0][1].cat(v)  # value
+    mx.eval(test_kv_cache.sliced[0][0], test_kv_cache.sliced[0][1])
+    toc = time.perf_counter()
+    tpi = 1e3 * (toc - tic) / max_len
+    print(f"test_kv Time per iteration {tpi:.3f} (ms)")
+
+
+def benchmark_array_assignment():
+    n_kv_heads = 32
+    head_dim = 4096 // n_kv_heads
+
+    # Low dims
+    a = mx.zeros([1, n_kv_heads, 100, head_dim])
+    b = mx.ones([1, n_kv_heads, 1, head_dim])
+    mx.eval(a, b)
+    tic = time.perf_counter()
+    for i in range(100):
+        a[..., i : i + 1, :] = b
+        mx.eval(a)
+    toc = time.perf_counter()
+    tpi = 1e3 * (toc - tic) / 100
+    print(f"Low dims Time per iteration {tpi:.3f} (ms)")
+
+    # High dims
+    a = mx.zeros([1, 2, 1, n_kv_heads, 100, head_dim])
+    b = mx.ones([1, n_kv_heads, 1, head_dim])
+    mx.eval(a, b)
+    tic = time.perf_counter()
+    for i in range(100):
+        a[0, 0, :, :, i : i + 1, :] = b
+        mx.eval(a)
+    toc = time.perf_counter()
+    tpi = 1e3 * (toc - tic) / 100
+    print(f"High dims Time per iteration {tpi:.3f} (ms)")
+
+
+if __name__ == "__main__":
+    print("** Comparing KV cache cat **")
+    benchmark_kv_cache_cat()
+    print("** Comparing array assignment **")
+    benchmark_array_assignment()
