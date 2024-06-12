82674b99615a80222735acc2c353aed196c4f710
Yi Wang <yi.wang.2005@gmail.com>
2024-06-12 08:08:42 -0700

    Add time_mlx_test.py (#81)
    * Add time_mlx_test.py
    
    * Allow 1 ms error
    
    ---------
    
    Co-authored-by: Yi Wang <yi.wang.2005@gmail.com>

---
+++ time_mlx_test.py
@@ -0,0 +1,43 @@
+import math
+
+import mlx
+import mlx.core as mx
+
+from . import time_mlx
+
+
+@time_mlx.function("two_projs")
+def two_projs():
+    DIM = 1024
+
+    @time_mlx.function("create_projs")
+    def create_projs():
+        p1 = mlx.nn.Linear(DIM, DIM, bias=False)
+        p2 = mlx.nn.Linear(DIM, DIM, bias=False)
+        return p1, p2
+
+    @time_mlx.function("run_projs")
+    def run_projs(p1, p2):
+        x = mx.ones((1024, 1024))
+        return p2(p1(x))
+
+    p1, p2 = create_projs()
+    run_projs(p1, p2)
+
+
+def test_time_mlx_two_projs():
+    for _ in range(10):
+        time_mlx.clear_ledger()
+        two_projs()
+        assert (
+            time_mlx.ledger["create_projs"][0] + time_mlx.ledger["run_projs"][0]
+            < time_mlx.ledger["two_projs"][0]
+        )
+        assert (
+            math.fabs(
+                time_mlx.ledger["create_projs"][0]
+                + time_mlx.ledger["run_projs"][0]
+                - time_mlx.ledger["two_projs"][0]
+            )
+            < 1  # ms
+        )
