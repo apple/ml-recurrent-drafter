32330d5f65a3934830c4107268329d78762a780f
Yi Wang <yi_wang999747@apple.com>
2024-09-11 21:44:22 -0700

    Run vicuna 4bit (#20)
    * slight change benchmark name
    
    * Make benchmark_autoregression.py support 4bit
    
    * Update
    
    * Update
    
    * Fix 4bit
    
    ---------
    
    Co-authored-by: Yi Wang <yi.wang.2005@gmail.com>

--- recurrent_drafting/mlx/experiments/benchmark_autoregression.py
+++ recurrent_drafting/mlx/experiments/benchmark_autoregression.py
@@ -19,6 +19,8 @@
 """
 import itertools
 import os
+import sys
+from typing import Optional
 
 import mlx
 import mlx.core as mx
@@ -30,6 +32,7 @@ import transformers
 import recurrent_drafting.mlx
 
 MODEL_PATH = os.path.expanduser("~/m/vicuna-7b-v1.3-bf16")
+QUANTIZED_MODEL_PATH = os.path.expanduser("~/m/vicuna-7b-v1.3-4bit")
 
 tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_PATH)
 new_ids = tokenizer(
@@ -41,7 +44,7 @@ new_ids = tokenizer(
 PROMPT = mx.array([new_ids])  # batch size = 1
 
 
-def benchmark_our_model(dtype: mx.Dtype, num_new_tokens: int) -> None:
+def benchmark_our_model(dtype: Optional[mx.Dtype], num_new_tokens: int) -> None:
     @recurrent_drafting.mlx.time_mlx.function("test process prompt takes")
     def process_prompt(prompt, our_model, sliced_cache):
         mask = recurrent_drafting.mlx.attention.causal_mask(
@@ -52,58 +55,68 @@ def benchmark_our_model(dtype: mx.Dtype, num_new_tokens: int) -> None:
 
     @recurrent_drafting.mlx.time_mlx.function(f"test generate {num_new_tokens} tokens takes")
     def generate_tokens(logits, model, cache, prompt_len):
+        print("\nour model generates:", file=sys.stderr)
         for i in range(num_new_tokens - 1):
             y = mx.argmax(mx.softmax(logits), axis=-1)
+            print(y.item(), end=", ", file=sys.stderr)
             mask = recurrent_drafting.mlx.attention.causal_mask(
                 mx.ones(shape=(1, prompt_len + i + 1), dtype=mx.bool_), 1
             )
             logits = model(y[None], 1, mask, cache)[1][:, -1, :]
         return mx.argmax(logits, axis=-1)
 
-    our_model = recurrent_drafting.mlx.modeling_llama.load_model(MODEL_PATH)
-    our_model.set_dtype(dtype)
+    our_model = recurrent_drafting.mlx.modeling_llama.load_model(
+        MODEL_PATH if dtype is not None else QUANTIZED_MODEL_PATH
+    )
+    if dtype is not None:
+        our_model.set_dtype(dtype)
     cache = recurrent_drafting.mlx.kv_cache.Cache(
         batch_size=1,
         max_length=PROMPT.shape[1] + num_new_tokens,
         n_layers=our_model.args.num_hidden_layers,
         n_heads=our_model.args.num_key_value_heads or our_model.args.num_attention_heads,
         head_dim=our_model.args.hidden_size // our_model.args.num_attention_heads,
-        dtype=our_model.model.embed_tokens.weight.dtype,
+        dtype=mx.float16,
     )
     logits = process_prompt(PROMPT, our_model, cache.sliced)
     generate_tokens(logits, our_model, cache.sliced, PROMPT.shape[1])
 
 
-def benchmark_mlxlm_model(dtype: mx.Dtype, num_new_tokens: int) -> None:
+def benchmark_mlxlm_model(dtype: Optional[mx.Dtype], num_new_tokens: int) -> None:
     @recurrent_drafting.mlx.time_mlx.function("ref prompt processing takes")
     def process_prompt(prompt, model, cache):
         return model(prompt, cache=cache)[:, -1, :]
 
     @recurrent_drafting.mlx.time_mlx.function(f"ref generate {num_new_tokens} tokens takes")
     def generate_tokens(logits, model, cache):
+        print("\nmlx_lm model generates:", file=sys.stderr)
         for _ in range(num_new_tokens - 1):
             y = mx.argmax(mx.softmax(logits), axis=-1)
+            print(y.item(), end=", ", file=sys.stderr)
             logits = model(y[None], cache)[:, -1, :]
         return mx.argmax(logits, axis=-1)
 
-    mlxlm_model, _ = mlx_lm.utils.load(MODEL_PATH)
-    mlxlm_model.set_dtype(dtype)
-    ref_cache = [
+    mlxlm_model, _ = mlx_lm.utils.load(MODEL_PATH if dtype is not None else QUANTIZED_MODEL_PATH)
+    if dtype is not None:
+        mlxlm_model.set_dtype(dtype)
+    mlxlm_cache = [
         mlx_lm.models.base.KVCache(
             mlxlm_model.args.hidden_size // mlxlm_model.args.num_attention_heads,
             mlxlm_model.args.num_key_value_heads,
         )
         for _ in range(mlxlm_model.args.num_hidden_layers)
     ]
-    logits = process_prompt(PROMPT, mlxlm_model, ref_cache)
-    generate_tokens(logits, mlxlm_model, ref_cache)
+    logits = process_prompt(PROMPT, mlxlm_model, mlxlm_cache)
+    generate_tokens(logits, mlxlm_model, mlxlm_cache)
 
 
 if __name__ == "__main__":
     print(
         "run,dtype,comprehension_mlx,generation_mlx,comprehension_ours,generation_ours"
     )  # table header
-    for run, dtype, num_new_tokens in itertools.product(range(2), [mx.float16, mx.bfloat16], [200]):
+    for run, dtype, num_new_tokens in itertools.product(
+        range(1), [mx.float16, mx.bfloat16, None], [200]
+    ):
         mlx.core.metal.clear_cache()
         recurrent_drafting.mlx.time_mlx.ledger.reset()
         benchmark_mlxlm_model(dtype, num_new_tokens)
@@ -118,7 +131,8 @@ if __name__ == "__main__":
         comprehension_ours = recurrent_drafting.mlx.time_mlx.ledger.records[0].timing[0]
         generation_ours = recurrent_drafting.mlx.time_mlx.ledger.records[1].timing[0]
 
+        dtype_name = {mx.float16: "fp16", mx.bfloat16: "bf16", None: "q4"}
         print(
-            f"{run},{dtype},{comprehension_mlx},{generation_mlx},"
+            f"{run},{dtype_name[dtype]},{comprehension_mlx},{generation_mlx},"
             + f"{comprehension_ours},{generation_ours}"
         )
--- recurrent_drafting/mlx/experiments/vicuna_4bit.py
+++ recurrent_drafting/mlx/experiments/vicuna_4bit.py
@@ -1,32 +1,76 @@
 import os
 
+import mlx.core as mx
 import mlx_lm
 import torch
 import transformers
 
 import recurrent_drafting
+import recurrent_drafting.mlx
 
 PYTORCH_BIN_REPO = "lmsys/vicuna-7b-v1.3"
 SAFETENSOR_FP16_DIR = os.path.expanduser("~/m/vicuna-7b-v1.3-fp16")
 SAFETENSOR_4BIT_DIR = os.path.expanduser("~/m/vicuna-7b-v1.3-4bit")
 
+NUM_NEW_TOKENS = 200
 
-if not os.path.isdir(SAFETENSOR_4BIT_DIR):
-    if not os.path.isdir(SAFETENSOR_FP16_DIR):
-        model = transformers.AutoModelForCausalLM.from_pretrained(
-            PYTORCH_BIN_REPO, torch_dtype=torch.float16
+# QUESTION = "When was Apple Inc. founded?"
+QUESTION = "Write a story about Einstein."
+
+
+def quantize_model_if_not():
+    if not os.path.isdir(SAFETENSOR_4BIT_DIR):
+        if not os.path.isdir(SAFETENSOR_FP16_DIR):
+            model = transformers.AutoModelForCausalLM.from_pretrained(
+                PYTORCH_BIN_REPO, torch_dtype=torch.float16
+            )
+            tkner = transformers.AutoTokenizer.from_pretrained(PYTORCH_BIN_REPO)
+            model.save_pretrained(SAFETENSOR_FP16_DIR, push_to_hub=False)
+            tkner.save_pretrained(SAFETENSOR_FP16_DIR, push_to_hub=False)
+        mlx_lm.convert(SAFETENSOR_FP16_DIR, mlx_path=SAFETENSOR_4BIT_DIR, quantize=True)
+
+
+def mlx_generate():
+    model, tokenizer = mlx_lm.load(SAFETENSOR_4BIT_DIR)
+    prompt = recurrent_drafting.chat.vicuna_prompt([QUESTION], add_generation_prompt=True)
+    for t in mlx_lm.stream_generate(model, tokenizer, prompt, max_tokens=NUM_NEW_TOKENS):
+        print(t, end="", flush=True)
+    print()
+
+
+def our_generate():
+    model = recurrent_drafting.mlx.modeling_llama.load_model(SAFETENSOR_4BIT_DIR)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(SAFETENSOR_FP16_DIR)
+    p = recurrent_drafting.chat.vicuna_prompt([QUESTION], add_generation_prompt=True)
+    ids = tokenizer(p).input_ids
+    prompt = mx.array([ids])
+
+    cache = recurrent_drafting.mlx.kv_cache.Cache(
+        batch_size=1,
+        max_length=prompt.shape[1] + NUM_NEW_TOKENS,
+        n_layers=model.args.num_hidden_layers,
+        n_heads=model.args.num_key_value_heads or model.args.num_attention_heads,
+        head_dim=model.args.hidden_size // model.args.num_attention_heads,
+        dtype=mx.float16,
+    )
+
+    mask = recurrent_drafting.mlx.attention.causal_mask(
+        mx.ones(shape=(1, prompt.shape[1]), dtype=mx.bool_), prompt.shape[1]
+    )
+    logits = model(prompt, prompt.shape[1], mask, cache.sliced)[1][:, -1, :]
+    for i in range(NUM_NEW_TOKENS - 1):
+        y = mx.argmax(mx.softmax(logits), axis=-1)
+        print(tokenizer._convert_id_to_token(y.item()), end="")
+        mask = recurrent_drafting.mlx.attention.causal_mask(
+            mx.ones(shape=(1, prompt.shape[1] + i + 1), dtype=mx.bool_), 1
         )
-        tkner = transformers.AutoTokenizer.from_pretrained(PYTORCH_BIN_REPO)
-        model.save_pretrained(SAFETENSOR_FP16_DIR, push_to_hub=False)
-        tkner.save_pretrained(SAFETENSOR_FP16_DIR, push_to_hub=False)
-    mlx_lm.convert(SAFETENSOR_FP16_DIR, mlx_path=SAFETENSOR_4BIT_DIR, quantize=True)
+        logits = model(y[None], 1, mask, cache.sliced)[1][:, -1, :]
+    y = mx.argmax(logits, axis=-1)
+    print(tokenizer._convert_id_to_token(y.item()))
 
 
-model, tokenizer = mlx_lm.load(SAFETENSOR_4BIT_DIR)
-PROMPT = recurrent_drafting.chat.vicuna_prompt(
-    ["Write a story about Einstein."], add_generation_prompt=True
-)
-print(PROMPT)
-for t in mlx_lm.stream_generate(model, tokenizer, PROMPT, max_tokens=512):
-    print(t, end="", flush=True)
-print()
+quantize_model_if_not()
+print("MLX generate:")
+mlx_generate()
+print("Our generate:")
+our_generate()
